5abba93fa47ed380db05db1136a8672532d9305f
Joao <j_donizetti@outlook.com>
2023-08-07 22:39:31 -0300

    First commit

---
+++ ex-01/01.js
@@ -0,0 +1,27 @@
+//Capturando os elementos do HTML
+
+const baseInput = document.getElementById('base');
+const alturaInput = document.getElementById('altura');
+const calcularBtn = document.getElementById('calcular');
+const resultadoDiv = document.getElementById('resultado');
+
+//Adicionar evento ao botão de calcular
+
+calcularBtn.addEventListener('click', () =>{
+    //convertendo entrada de valores para numeros
+    const base = parseFloat(baseInput.value);
+    const altura = parseFloat(alturaInput.value);
+
+    //verificando se são valores validos
+    if(isNaN(base) || isNaN(altura)){
+        resultadoDiv.textContent = 'Por favor insira um valor válido';
+        return;
+    }
+
+    //calcular área do retangulo
+    const area = base * altura;
+
+    //exibindo resultado
+    resultadoDiv.textContent = `A área do retangulo é de ${area.toFixed(1)}²`;
+
+});
