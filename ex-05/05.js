79fcf8796392ccd0151d61fb305e0167336e2900
Joao <j_donizetti@outlook.com>
2023-08-10 14:11:08 -0300

    Adicionando o ex-05

---
+++ ex-05/05.js
@@ -0,0 +1,29 @@
+
+//METODO-1
+//Criando as Arrays
+const array1 = [1,2,3,4,5];
+const array2 = ['joao', 'apollo', 'elisa'];
+
+//Juntando as arrays utilizando uma função 
+function concatenandoArrays() {
+    const arraysConcatenadas = [...array1, ...array2];
+    console.log(arraysConcatenadas);
+}
+
+concatenandoArrays();
+
+// //METODO-2 
+// //criando uma função para concatenar as arrays
+// function concatenandoArrays(array1, array2) {
+//     //utilizando o return + o .concat para retornar os arrays concatenados
+//     return array1.concat(array2);
+// }
+
+// //Criando os Arrays
+// const array1 = ['joao', 'apollo', 'elisa'];
+// const array2 = [1,2,3,4,5];
+// //Criando uma const para mostrar o resultado da concatenação 
+// const resultadoConcatenacao = concatenandoArrays(array1, array2);
+
+// //mostrando o resultado no console. 
+// console.log(resultadoConcatenacao);
