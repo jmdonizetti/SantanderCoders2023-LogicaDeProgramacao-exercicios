c44ae00abd697536fb061c04ca9fe7b4a6ef38fa
Joao <j_donizetti@outlook.com>
2023-08-15 19:33:46 -0300

    Adicionando ex-07

--- ex-05/05.js
+++ ex-05/05.js
@@ -2,29 +2,29 @@
 
 //METODO-1
 //Criando as Arrays
-const array1 = [1,2,3,4,5];
-const array2 = ['joao', 'apollo', 'elisa'];
-
-//Juntando as arrays utilizando uma função 
-function concatenandoArrays() {
-    const arraysConcatenadas = [...array1, ...array2];
-    console.log(arraysConcatenadas);
-}
-
-concatenandoArrays();
-
-// //METODO-2 
-// //criando uma função para concatenar as arrays
-// function concatenandoArrays(array1, array2) {
-//     //utilizando o return + o .concat para retornar os arrays concatenados
-//     return array1.concat(array2);
+// const array1 = [1,2,3,4,5];
+// const array2 = ['joao', 'apollo', 'elisa'];
+ 
+// //Juntando as arrays utilizando uma função 
+// function concatenandoArrays() {
+//     const arraysConcatenadas = [...array1, ...array2];
+//     console.log(arraysConcatenadas);
 // }
 
-// //Criando os Arrays
-// const array1 = ['joao', 'apollo', 'elisa'];
-// const array2 = [1,2,3,4,5];
-// //Criando uma const para mostrar o resultado da concatenação 
-// const resultadoConcatenacao = concatenandoArrays(array1, array2);
+// concatenandoArrays();
 
-// //mostrando o resultado no console. 
-// console.log(resultadoConcatenacao);
+//METODO-2 
+//criando uma função para concatenar as arrays
+function concatenandoArrays(array1, array2) {
+    //utilizando o return + o .concat para retornar os arrays concatenados
+    return array1.concat(array2);
+}
+
+//Criando os Arrays
+const lista1 = ['joao', 'apollo', 'elisa'];
+const lista2 = [1,2,3,4,5];
+//Criando uma const para mostrar o resultado da concatenação 
+const resultadoConcatenacao = concatenandoArrays(lista1, lista2);
+
+//mostrando o resultado no console. 
+console.log(resultadoConcatenacao);
