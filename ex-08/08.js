0d13d77f779672fce1e51124c32a349aae14a160
Joao <j_donizetti@outlook.com>
2023-08-15 20:22:51 -0300

    adicionando EX-08

---
+++ ex-08/08.js
@@ -0,0 +1,13 @@
+const lista1 = [1,2,3,4,5];
+const lista2 = [6,7,8,9,10];
+const lista3 = [11,12,13,14,15];
+const lista4 = [16,17,18,19,20];
+
+const maiorValorArray = Math.max(...lista1, ...lista2, ...lista3, ...lista4)
+
+//Printando o mario valor entre todas as lista 
+console.log(`Maior valor de todas as lista é: ${maiorValorArray}`);
+
+//printando o maior valor da primeira lista
+console.log(`Maior valor da primeira lista é: ${Math.max(...lista1)}`);
+
