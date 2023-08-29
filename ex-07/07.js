750bcc2af0084d694e0d55367482eeb2b3025ada
Joao <j_donizetti@outlook.com>
2023-08-29 20:13:49 -0300

    commit

--- ex-07/07.js
+++ ex-07/07.js
@@ -2,13 +2,12 @@
 
 // function calendario() {
 //     const data = new Date();
-//     const dia = data.getDay();
-//     const mes = data.getMonth();
+//     const dia = data.getDate();
+//     const mes = data.getMonth() + 1;
 //     const ano = data.getFullYear();
 //     const hora = data.getHours();
 //     const minuto = data.getMinutes();
 //     const segundos = data.getSeconds();
-
 
 //     //Utilizando ternario que vai adicionar o zero a esquerda para numeros menores que Dez... EX: ${dia < 10 ? "0" + dia : dia} de vez de sair 5 vai sair 05
 //     console.log(`hoje é dia ${dia < 10 ? "0" + dia : dia}/${mes < 10 ? "0" + mes : mes}/${ano}, são ${hora < 10 ? "0" + hora : hora}:${minuto < 10 ? "0" + minuto : minuto}:${segundos < 10 ? "0" + segundos : segundos}`);
